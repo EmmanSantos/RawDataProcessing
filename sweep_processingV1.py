832778f446c70ef4d74e5691d9e777d351fc90b1
Ehhmann <jes.santosph@gmail.com>
2024-07-02 16:12:27 +0800

    Baseline correction show dbm and mw

--- sweep_processingV1.py
+++ sweep_processingV1.py
@@ -2,14 +2,20 @@ import os
 # import cleanupV2_1_module
 import cleanupV2_2_unorderedWL
 import matplotlib.pyplot as plt
+import numpy as np
+import pybaselines
 
+def logtomw(input_array):
+    for i in range(0,len(input_array)):
+        input_array[i]=10**(input_array[i]/10)
 
+    return input_array
 
 #create directories
 csv_dir = input("Enter the folder name of the raw CSV (folder must be in same folder as exe): ")
 
 # csv_dir = "./unprocessedUpdate"
-processed_dir = './'+csv_dir+"-PROCESSED"
+processed_dir = './'+csv_dir+"-PROCESSED_baseline"
 graph_dir = processed_dir
 processed_dir = './'+csv_dir+"-PROCESSED/processed_csv"
 
@@ -101,8 +107,35 @@ for (path, names, fnames) in os.walk(csv_dir):
         # plt.show()
         plt.close()
 
-        
+        np_power = np.array(ave_power_plot)
+        # power_bsln_corr = pybaselines.Baseline.asls(ave_power_plot)
+        power_bsln = pybaselines.polynomial.imodpoly(ave_power_plot,poly_order=3)[0]
+        print(len(power_bsln))
+        bl_corrected_power = np.subtract(np_power,power_bsln)
+        plt.figure(figsize=(15,7))
+        plt.plot(ave_wl_plot,bl_corrected_power)
+        plt.plot(ave_wl_plot,ave_power_plot)
+        plt.title(name+"_baseline_corrected")
+        plt.xlabel("Wavelength (nm)")
+        plt.ylabel("Output Power (dBm)")
+        plt.grid(alpha=0.7)
+        # plt.savefig(processed_dir+"/"+name+"_baseline_corrected.png")
+        # plt.show()
+        # plt.close()
 
+        
+        bl_corrected_power_mw = logtomw(bl_corrected_power)
+        ave_power_plot_mw = logtomw(ave_power_plot)
+        plt.figure(figsize=(15,7))
+        plt.plot(ave_wl_plot,bl_corrected_power_mw)
+        plt.plot(ave_wl_plot,ave_power_plot_mw)
+        plt.title(name+"_baseline_corrected_mw")
+        plt.xlabel("Wavelength (nm)")
+        plt.ylabel("Output Power (mW)")
+        plt.grid(alpha=0.7)
+        # plt.savefig(processed_dir+"/"+name+"_baseline_corrected.png")
+        plt.show()
+        plt.close()
 input("Enter to Continue")
         
 
